9a625a58bc1daa2465f31c1cf55988be277df9fd
Zain <158439852+ZainAssist@users.noreply.github.com>
2024-02-02 07:11:35 +0530

    Create __init__.py

---
+++ Zain/__init__.py
@@ -0,0 +1,48 @@
+import asyncio
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram import Client,filters
+from pyrogram.types import *
+from kisan import config
+import logging
+from pyrogram.errors import (
+    ChatAdminRequired
+)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+if config.BOT_TOKEN:
+  @bot.on_message(filters.command("banall"))
+  async def _(bot, msg):
+    print("getting memebers from {}".format(msg.chat.id))
+    async for i in bot.iter_chat_members(msg.chat.id):
+        try:
+            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
+            print("kicked {} from {}".format(i.user.id,msg.chat.id))
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            print(e)
+        except Exception as e:
+            print(" failed to kicked {} from {}".format(i.user.id,e))           
+    print("process completed")
+
+
+if config.BOT_TOKEN:
+  @bot.on_message(filters.command(["start"]))
+  async def hello(bot, message):
+  await message.reply_photo(photo=f"https://graph.org/file/1256e1bed70a8b109615e.jpg",
+                              caption=f"ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ʙᴀɴ ᴀʟʟ ʙᴏᴛ ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙᴇʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ɪɴ ᴀ ғᴇᴡ  sᴇᴄᴏɴᴅs!\n\nᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪʙ me ғᴜʟʟ ᴘᴏᴡᴇʀs\n\ntype /banall to see magic",
+
+  reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}")
+                ]
+                
+           ]
+        ),
+    )
